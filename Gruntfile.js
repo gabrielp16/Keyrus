fff6dbee249045bcac39a9893464338c13665eb0
Gabriel <juan.pena@arandasoft.com>
2020-02-17 14:31:15 -0500

    Creacion de tareas para GruntJS

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var src = 'src';
+var dest = 'dist';
+
 module.exports = function (grunt) {
 
     grunt.initConfig({
@@ -7,17 +10,55 @@ module.exports = function (grunt) {
         concat: {
             dist: {
                 src: [
-                    'js/libs/*.js',
-                    '/js/global.js'
+                    'js/libs/*.js', // All JS in the libs folder
+                    '/js/global.js' // This specific file
                 ],
                 dest: '/js/build/production.js',
             }
-        }
+        },
+        uglify: {
+            build: {
+                src: '/js/build/production.js',
+                dest: '/js/build/production.min.js'
+            }
+        },
+        sass: { // Task
+            dist: { // Target
+                options: { // Target options
+                    style: 'expanded'
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'styles',
+                    src: ['*.scss'],
+                    dest: '../public',
+                    ext: '.css'
+                }]
+            }
+        },
+        watch: {
+            css: {
+                files: '**/*.sass',
+                tasks: ['sass']
+            }
+        },
+        availabletasks: {
+            tasks: {
+                options: {
+                    filter: 'exclude',
+                    tasks: ['availabletasks', 'tasks']
+                }
+            }
+        },
     });
 
-
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
 
+    grunt.loadNpmTasks('grunt-available-tasks');
 
-    grunt.registerTask('default', ['concat']);
+    grunt.registerTask('default', ['concat', 'uglify', 'sass', 'watch']);
+    grunt.registerTask('tasks', ['availabletasks']);
 };
